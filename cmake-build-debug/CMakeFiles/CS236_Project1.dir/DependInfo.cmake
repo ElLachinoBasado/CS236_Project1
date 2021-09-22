9e60ff2ea0d5641c7147916da75a9a2251b17de1
Helaman Sung <helamanjungminsung@gmail.com>
2021-09-22 02:20:10 -0600

    Made minor edits based on test cases. Everything is finished. :) (Hopefully)

---
+++ cmake-build-debug/CMakeFiles/CS236_Project1.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/BlockCommentAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/BlockCommentAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/ColonAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/ColonAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/ColonDashAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/ColonDashAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/FactsAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/FactsAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/IdAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/IdAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/Lexer.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/Lexer.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/LineCommentAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/LineCommentAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/MatcherAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/MatcherAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/QueriesAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/QueriesAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/RulesAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/RulesAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/SchemesAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/SchemesAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/StringAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/StringAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/Token.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/Token.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/UnclosedBlockCommentAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/UnclosedBlockCommentAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/UnclosedStringAutomaton.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/UnclosedStringAutomaton.cpp.obj"
+  "C:/Users/Helaman/CLionProjects/CS236_Project1/main.cpp" "C:/Users/Helaman/CLionProjects/CS236_Project1/cmake-build-debug/CMakeFiles/CS236_Project1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CS236_Project1.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS236_Project1.dir/BlockCommentAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/ColonAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/ColonDashAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/FactsAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/IdAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/Lexer.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/LineCommentAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/MatcherAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/QueriesAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/RulesAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/SchemesAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/StringAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/Token.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/UnclosedBlockCommentAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/UnclosedStringAutomaton.cpp.obj"
+  "CMakeFiles/CS236_Project1.dir/main.cpp.obj"
+  "CS236_Project1.exe"
+  "CS236_Project1.exe.manifest"
+  "CS236_Project1.lib"
+  "CS236_Project1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS236_Project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
